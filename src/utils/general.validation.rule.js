80be4cfcabf9e949f689137088ed44fb4a27cf3b
OsamaIbrahim1 <oi5921435@gmial.com>
2024-10-12 19:58:21 +0300

    deployment in render

--- src/utils/general.validation.rule.js
+++ src/utils/general.validation.rule.js
@@ -3,7 +3,7 @@ import { Types } from "mongoose";
 
 const objectIdValidation = (value, helper) => {
   const isValid = Types.ObjectId.isValid(value);
-  return (isValid ? value : helper.message("invalid objectId"));
+  return isValid ? value : helper.message("invalid objectId");
 };
 
 export const generalRules = {
@@ -19,5 +19,30 @@ export const generalRules = {
     accept: Joi.string(),
     "accept-encoding": Joi.string(),
     connection: Joi.string(),
+  }),
+  hostHeaderRules: Joi.object({
+    accesstoken: Joi.string().required(),
+    "postman-token": Joi.string(),
+    "cache-control": Joi.string(),
+    host: Joi.string(),
+    "content-type": Joi.string(),
+    "content-length": Joi.string(),
+    "user-agent": Joi.string(),
+    accept: Joi.string(),
+    "accept-encoding": Joi.string(),
+    connection: Joi.string(),
+    "cdn-loop": Joi.string(),
+    "cf-connecting-ip": Joi.string(),
+    "cf-ew-via": Joi.string(),
+    "cf-ipcountry": Joi.string(),
+    "cf-ray": Joi.string(),
+    "cf-visitor": Joi.string(),
+    "cf-worker": Joi.string(),
+    "render-proxy-ttl": Joi.string(),
+    "rndr-id": Joi.string(),
+    "true-client-ip": Joi.string(),
+    "x-forwarded-for": Joi.string(),
+    "x-forwarded-proto": Joi.string(),
+    "x-request-start": Joi.string(),
   }),
 };
